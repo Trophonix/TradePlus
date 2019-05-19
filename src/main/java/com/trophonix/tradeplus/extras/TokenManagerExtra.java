85a28fb37fbd5eec3ac5a9e264c182af8075e209
Lucas <502badgamer@gmail.com>
2019-05-19 12:06:30 -0500

    3.31 - TokenManager extra offline mode support

--- src/main/java/com/trophonix/tradeplus/extras/TokenManagerExtra.java
+++ src/main/java/com/trophonix/tradeplus/extras/TokenManagerExtra.java
@@ -4,29 +4,33 @@ import com.trophonix.tradeplus.TradePlus;
 import com.trophonix.tradeplus.trade.Trade;
 import com.trophonix.tradeplus.util.ItemFactory;
 import me.realized.tokenmanager.TokenManagerPlugin;
+import me.realized.tokenmanager.api.TokenManager;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class TokenManagerExtra extends Extra {
-  
+
+  private TokenManager api;
+
   public TokenManagerExtra(Player player1, Player player2, TradePlus pl, Trade trade) {
     super("tokenmanager", player1, player2, pl, trade);
+    api = (TokenManager)pl.getServer().getPluginManager().getPlugin("TokenManager");
   }
 
   @Override
   public double getMax(Player player) {
-    return TokenManagerPlugin.getInstance().getTokens(player).orElse(0);
+    return api.getTokens(player).orElse(0);
   }
 
   @Override
   public void onTradeEnd() {
     if (value1 > 0) {
-      TokenManagerPlugin.getInstance().removeTokens(player1.getUniqueId().toString(), (long)value1);
-      TokenManagerPlugin.getInstance().addTokens(player2.getUniqueId().toString(), (long)value1);
+      api.setTokens(player1, api.getTokens(player1).orElse((long)value1) - (long)value1);
+      api.setTokens(player2, api.getTokens(player2).orElse(0L) + (long)value1);
     }
     if (value2 > 0) {
-      TokenManagerPlugin.getInstance().removeTokens(player2.getUniqueId().toString(), (long)value2);
-      TokenManagerPlugin.getInstance().addTokens(player1.getUniqueId().toString(), (long)value2);
+      api.setTokens(player2, api.getTokens(player2).orElse((long)value2) - (long)value2);
+      api.setTokens(player1, api.getTokens(player1).orElse(0L) + (long)value2);
     }
   }
 

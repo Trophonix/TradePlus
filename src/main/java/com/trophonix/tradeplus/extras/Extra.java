2671559af88ce46f66e25cfce0282101858629f2
Lucas Cosolo <lucascosolo@gmail.com>
2022-05-31 23:44:10 -0500

    Ensure trade is cancelled on quit

--- src/main/java/com/trophonix/tradeplus/extras/Extra.java
+++ src/main/java/com/trophonix/tradeplus/extras/Extra.java
@@ -104,11 +104,18 @@ public abstract class Extra implements Listener {
                         return this;
                       }
                       if (number.doubleValue() < 0 || number.doubleValue() > getMax(player)) {
-                        return new NumericPrompt() {
+                        return new StringPrompt() {
                           @Override
-                          protected Prompt acceptValidatedInput(
-                              ConversationContext conversationContext, Number number) {
+                          public Prompt acceptInput(
+                              ConversationContext conversationContext, String input) {
                             if (trade.isCancelled()) return null;
+                            if (input == null || input.equalsIgnoreCase("cancel")) return null;
+                            Number number;
+                            try {
+                              number = Double.parseDouble(input);
+                            } catch (NumberFormatException ignored) {
+                              return this;
+                            }
                             if (number.doubleValue() < 0 || number.doubleValue() > getMax(player)) {
                               return this;
                             }
